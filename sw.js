2f357050e4b9d4ddb5eb97e89fa27d7e8768bec1
GitHub ZUAKC户名 <GitHub 3024906354@qq.com>
2023-12-10 19:40:04 +0800

    Site updated: 2023-12-10 19:40:04

--- sw.js
+++ sw.js
@@ -10,7 +10,7 @@
 
 'use strict';
 
-var precacheConfig = [];
+var precacheConfig = [["/sw-register.js","fc230acefc4df43788aee5642bc1dcc1"]];
 var cacheName = 'sw-precache-v3--' + (self.registration ? self.registration.scope : '');
 var firstRegister = 1; // 默认1是首次安装SW， 0是SW更新
 
